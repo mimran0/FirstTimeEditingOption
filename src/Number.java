223663456112bc2f340192ab54620de33008e707
mimran0 <jmrthan@gmail.com>
2016-03-31 22:25:43 -0400

    4th time modification

---
+++ src/Number.java
@@ -0,0 +1,37 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		
+		int a;
+		
+		
+		
+		for (int i=1;i<10;i=i+1){
+			
+			
+			
+			for (int j=1;j<10;j=j+1){
+				
+				a=i/j;
+				//System.out.println(a);
+				
+				if (a!=1 & a!=0 ){ 
+			        
+					System.out.println("The number is: "+i);
+			    }
+				
+			}
+			
+			
+			 //System.out.println(a);
+		}
+		
+		
+		
+		//System.out.println(Result);
+
+	}
+
+}
---
+++ src/twegbqw.java
@@ -0,0 +1,21 @@
+
+public class twegbqw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	int v = 20; 
+	System.out.println(v);
+		--v; 
+		System.out.println(v);
+	System.out.println(v++);
+	System.out.println(v);
+
+//		int s = 20; 
+//		int t = s++ + --s;
+//		System.out.println(s);
+//		System.out.println(t);
+	}
+
+}
